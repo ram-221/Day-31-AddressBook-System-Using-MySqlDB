3d7ab5973969d6604a38c301f5f4e51744c7de37
ram-221 <rameshmalli857.com>
2022-10-17 16:26:29 +0530

    this is sample push

---
+++ Sample.sql
@@ -0,0 +1,5 @@
+create database bridgelabz;
+use bridgelabz;
+create table student(firstName varchar(255),lastName varchar(255));
+insert into student value("Ramachandra","Acharya");
+select * from student;
